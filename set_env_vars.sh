67d4b4890e140f31e8f0a3f298be456d20b9d2e8
thahir-reddy <thahir@ost.com>
2019-01-09 17:51:54 +0530

    website changes by Ignas

---
+++ set_env_vars.sh
@@ -0,0 +1,4 @@
+# Note this secret has to be same for company-web & company-api in order to make CSRF work
+# As FE uses staging ENV for local testing, this should be in sync with Staging API Secret
+export FOUNDATION_SECRET_KEY_BASE='b1f0ff90cd692556f9740a8e609f88f2f4fc15d9dda9035445a7577c3f94936eaae91a0793c4ad5500314fe5a526a3b3f7c7c71c303f883d903df138783a8225'
+
