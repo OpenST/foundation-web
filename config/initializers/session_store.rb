1b127744ae968b0c75e7bafcb5eb3516f0c7ad6e
Ashutosh <ashutosh@ost.com>
2019-03-19 16:04:23 +0530

    minor fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ost_session_id', domain: :all, http_only: true, secure: !Rails.env.development?, same_site: :strict
+Rails.application.config.session_store :cookie_store, key: '_ost_session_id', domain: :all, http_only: true, secure: Rails.env.production?, same_site: :strict
